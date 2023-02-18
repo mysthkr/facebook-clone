ba9f31a5baef4faf71689259f3fcc75c9c3622f2
mysthkr4036@gmail.com <hikaru_miyashita>
2023-02-18 11:51:10 +0900

    Create draft create contloller.

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,10 +1,29 @@
 class PicturesController < ApplicationController
   
+  def index
+    @pictures = Picture.all
+  end
+  
   def new
     @picture = Picture.new
   end
   
-  def index
-    @pictures = Picture.all
+  def create
+    @picture = Picture.new(post_params)
+    if params[:back]
+      render :new
+    else
+      if @picture.save
+        redirect_to pictures_path, notice: "投稿しました！"
+      else
+        render :new
+      end
+    end
+  end
+  
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
   end
 end

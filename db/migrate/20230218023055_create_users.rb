743823c0be5c6b3fbbb13529233a2c29f09b7d73
mysthkr4036@gmail.com <hikaru_miyashita>
2023-02-19 19:04:59 +0900

    Edit mg file

--- db/migrate/20230218023055_create_users.rb
+++ db/migrate/20230218023055_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration[6.1]
       t.string :name
       t.string :email
       t.string :password_digest
-      add_index :users, :email, unique: true
+
       t.timestamps
     end
   end
